0add63433464189f02defe773130203a8ba1c80f
Gadmerci <141875543+Gadmerci@users.noreply.github.com>
2023-12-02 08:55:41 +0200

    Add files via upload

---
+++ port.js
@@ -0,0 +1,21 @@
+//position of navbar
+ 
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if (this.scrollTop() > 20) {
+//             $(".navbar").addClass("sticky");
+//         } else {
+//             $(".navbar").removeClass("sticky");
+//         }
+//     });
+// });
+
+//typing animation 
+
+var typed = new Typed(".typing", {
+    strings: ["Web developer", "Programmer", "Backend", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
